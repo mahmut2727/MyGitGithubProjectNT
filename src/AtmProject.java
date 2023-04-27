374a26b0f54877a2411bd318124e8544fbb26234
Mahmut2727 <mahmutozmermer1907@gmail.com>
2023-04-27 13:24:10 +0300

    mhmt1

--- src/AtmProject.java
+++ src/AtmProject.java
@@ -8,5 +8,8 @@ public class AtmProject {
         //version3
         System.out.println("Git ogrenmek cok kolay");
         //version4
+
+        System.out.println("mahmut branchi kodlarini yazdi.");
+        //version mhmt1
     }
 }
